1d574ce11f9fb011602e20351fef6e377c66b346
OneTwoDan <danielondonoa@gmail.com>
2022-04-26 11:37:46 -0500

    second commit

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import "./landing.css";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <header>
+      <div className="container landing_container">
+        <h1 className="header-title">HACKATHON 2022</h1>
+        <h4>28 AL 30 DE ABRIL</h4>
+        <h2 className="header-title2">TAXIS LIBRES</h2>
+        <Link to="/register">
+          <button className="btn">REGISTRO</button>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
-export default Landing
+export default Landing;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import { FaHome } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { useState } from "react";
+
+import "./nav.css";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a
+        href="/#"
+        onClick={() => setActiveNav("/#")}
+        className={activeNav === "/#" ? "active" : ""}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#information"
+        onClick={() => setActiveNav("#information")}
+        className={activeNav === "#information" ? "active" : ""}
+      >
+        <FaUser />
+      </a>
+      <a
+        href="#sponsors"
+        onClick={() => setActiveNav("#sponsors")}
+        className={activeNav === "#sponsors" ? "active" : ""}
+      >
+        <FaBook />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
